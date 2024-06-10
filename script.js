1c2bf13e710bb5a9e42e7b56e485d439cbff17be
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-09 18:15:19 -0600

    Start coding a system to check if a player has won

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const newGame = (function() {
         c2: '', 
         c3: ''
     };
+
+    const checkWin = () => {
+        Object.keys(gameBoard).filter((e) => {
+            return e.indexOf('a') == 0;
+        }).reduce((keysA, e) => {
+            keysA[e] = gameBoard[e];
+            console.log(keysA);
+        }, {});
+    };
     const startRound = () => {
         let randomTurn = Math.floor(Math.random() * 2);
         if (randomTurn == 1) {
@@ -19,15 +28,15 @@ const newGame = (function() {
             // X is first
             playerTwo.turn = 4;
             // O
-            alert(`Player One is X! Player one goes first!`)
+            //alert(`Player One is X! Player one goes first!`)
         } else {
             playerOne.turn = 4;
             // O
             playerTwo.turn = 5;
             // X
-            alert(`Player Two is X! Player two goes first!`)
+            //alert(`Player Two is X! Player two goes first!`)
         }
-        for (let i in gameBoard) {
+       /* for (let i in gameBoard) {
                 if (gameBoard[i] == '') {
                     let turnX = prompt(`Player One: Enter the board position you wish to play: (example: B1) `);
                     gameBoard[turnX] = 'X';
@@ -38,9 +47,9 @@ const newGame = (function() {
                     gameBoard[turnO] = 'O';
                     alert(`O placed: Game Board: ${ JSON.stringify(gameBoard) }!`);
                 }
-            }
+            }*/
     }
-    return { playerOne, playerTwo, gameBoard, startRound}
+    return { playerOne, playerTwo, gameBoard, startRound, checkWin}
 })();
 
-newGame.startRound()
+newGame.checkWin()
